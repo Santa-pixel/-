1faa779631940072da834e3114840c5cbfa6f093
santa <santa3302@yandex.ru>
2020-04-26 20:59:51 +0300

    Добавьте файлы проекта.

---
+++ Прпвктика/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Прпвктика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int count;
+            //for (count = 0; count < 5; count++)
+            //    Console.WriteLine(count);
+
+            // Преминение кодового блока
+            //int a, b, c;
+            //a = 5;
+            //b = 10;
+            //if(a != 0)
+            //{
+            //    Console.WriteLine("i не равно 0");
+            //    c = b / a;
+            //    Console.WriteLine("b / a = " + c);
+
+            //Вычисление суммы и произведения чисел от 1 до 10
+            //int prod;
+            //int sum;
+            //int i;
+
+            //sum = 0;
+            //prod = 1;
+            //for(i =1; i <=10; i++)
+            //{
+            //    sum = sum + i;
+            //    prod = prod * i;
+            //}
+            //Console.WriteLine("Сумма всех чисел равна " + sum);
+            //Console.WriteLine("роизведение всех чисел равно " + prod);
+
+            // Вычислить расстояние от земли до солнца в сантиметрах
+            //long kilometer;
+            //long centimetre;
+
+            //kilometer = 147000000;
+            //centimetre = kilometer * 1000 * 100;
+            //Console.WriteLine("Расстояние до солнца" + centimetre + " сантимеров.");
+
+            //byte x;
+            //int sum;
+
+            //sum = 0;
+
+            //for (x = 1; x <= 100; x++)
+            //    sum = sum + x;
+            //Console.WriteLine("Сумма чисел от 1 до 100 = " + sum);
+
+            //// Определить радиус окружности по площади круга
+            //Double r;
+            //Double area;
+            //area = 10.0;
+            //r = Math.Sqrt(area / 3.1416);
+            //Console.WriteLine("Радиус =" + r);
+
+            //Double theta; // угол в радианах
+
+            //for(theta = 0.1; theta <= 1.0; theta = theta + 0.1)
+            //{
+            //    Console.WriteLine("Синус угла " + theta + "равен" + Math.Sin(theta));
+            //    Console.WriteLine("Косинус угла " + theta + "равен" + Math.Cos(theta));
+            //    Console.WriteLine("Тангенсугла " + theta + "равен" + Math.Tan(theta));
+            //}
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
